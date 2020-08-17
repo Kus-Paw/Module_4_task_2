9798c7119d6fa90b8a1f39b39b092ca427626a2d
Kus-Paw <68556631+Kus-Paw@users.noreply.github.com>
2020-08-17 18:03:21 +0200

    fourth comit

--- Moduł_4_zad_2_.py
+++ Moduł_4_zad_2_.py
@@ -2,44 +2,30 @@ import sys
 import logging
 logging.basicConfig(level=logging.DEBUG)
 
-print("Podaj działanie, posługując się odpowiednią liczbą: 1 Dodawanie, 2 Odejmowanie, 3 Mnożenie, 4 Dzielenie:")
-def summary():
-    dodawanie = number_1 + number_2
-    print ("Wynik to:", dodawanie)
-def subtraction():
-    odejmowanie = number_1 - number_2
-    print ("Wynik to:", odejmowanie)
-def multiplication():
-    mnożenie = number_1 * number_2
-    print ("Wynik to:", mnożenie)
-def split():
-    dzielenie = number_1 / number_2
-    print ("Wynik to:", dzielenie)
+if __name__ == "__main__":
+    def users_input ():
+        print("Podaj działanie, posługując się odpowiednią liczbą: 1 Dodawanie, 2 Odejmowanie, 3 Mnożenie, 4 Dzielenie:")
+        choice = input ("Wciśnij odpowiedni klawisz: ")
+        number_1 = float(input("Podaj składnik 1: "))
+        number_2 = float(input("Podaj składnik 2: "))
+        operations (number_1,number_2, choice)
+        return
+   
+    def operations(number_1, number_2, choice):
+        if (choice == "1"):
+            logging.info ("Dodaję", number_1 + number_2)
+        
+        elif (choice == "2"):
+            logging.info ("Odejmuję", number_1 - number_2)
+            
+        elif (choice == "3"):
+            logging.info ("Mnoże", number_1 * number_2)
+            
+        elif (choice == "4"):
+            logging.info ("Wynik to:", number_1 / number_2)
 
-choice = input ("Wciśnij odpowiedni klawisz: ")
-# jeżeli zorbie funkcje pobierania danych od użytkownika np. def number_choices():
-# number_1 = float(input("Podaj składnik 1 "))
-# number_2 = float(input("Podaj składnik 2 "))
-# to mam problem aby wywołać tylko wynik to co wpisał uzytkownik w logging.info ("Dodaję ", number_1, "i ", number_2)
-
-number_1 = float(input("Podaj składnik 1 "))
-number_2 = float(input ("Podaj sładnik 2 "))
-if (choice == "1"):
-    logging.info ("Dodaję ", number_1, "i ", number_2)
-    print(summary())
-elif (choice == "2"):
-    logging.info ("Odejmuję", number_1, "i",  number_2)
-    print(subtraction())
-elif (choice == "3"):
-    logging.info ("Mnoże", number_1, "i", number_2)
-    print(multiplication())
-elif (choice == "4"):
-    logging.info ("Dzielę", number_1, "i", number_2)
-    print(split())
-
-logging.info("The program was called with this parameters %s" % choice)
-logging.debug("First parameter is %s" % number_1)
-logging.debug("Second parameter is %s" % number_2)
-    
-logging.basicConfig(level=logging.DEBUG, format='%(asctime)f %(message)f', filename="logfile.log")
-
+        logging.info("The program was called with this parameters %s" % choice)
+        logging.debug("First parameter is %s" % number_1)
+        logging.debug("Second parameter is %s" % number_2)
+        logging.basicConfig(level=logging.DEBUG, format='%(asctime)f %(message)f', filename="logfile.log")
+users_input()
