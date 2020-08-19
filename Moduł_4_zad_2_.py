ebb23f72df11d27a391ec7f2b5ce541ca3cf059b
Kus-Paw <68556631+Kus-Paw@users.noreply.github.com>
2020-08-19 20:09:30 +0200

    fifth comit

--- Moduł_4_zad_2_.py
+++ Moduł_4_zad_2_.py
@@ -1,31 +1,55 @@
-import sys
+from functools import reduce
 import logging
 logging.basicConfig(level=logging.DEBUG)
 
-if __name__ == "__main__":
-    def users_input ():
-        print("Podaj działanie, posługując się odpowiednią liczbą: 1 Dodawanie, 2 Odejmowanie, 3 Mnożenie, 4 Dzielenie:")
-        choice = input ("Wciśnij odpowiedni klawisz: ")
-        number_1 = float(input("Podaj składnik 1: "))
-        number_2 = float(input("Podaj składnik 2: "))
-        operations (number_1,number_2, choice)
-        return
-   
-    def operations(number_1, number_2, choice):
-        if (choice == "1"):
-            logging.info ("Dodaję", number_1 + number_2)
-        
-        elif (choice == "2"):
-            logging.info ("Odejmuję", number_1 - number_2)
-            
-        elif (choice == "3"):
-            logging.info ("Mnoże", number_1 * number_2)
-            
-        elif (choice == "4"):
-            logging.info ("Wynik to:", number_1 / number_2)
+def add(a, b, *args):
+    """add 2 or more arguments"""
+    logging.info(f"Dodaję: {a}, {b}, {','.join(map(str, args))}")
+    return a + b + sum(args)
 
-        logging.info("The program was called with this parameters %s" % choice)
-        logging.debug("First parameter is %s" % number_1)
-        logging.debug("Second parameter is %s" % number_2)
-        logging.basicConfig(level=logging.DEBUG, format='%(asctime)f %(message)f', filename="logfile.log")
-users_input()
+def sub(a, b, *args):
+    logging.info(f"Odejmuję: {a}, {b}, {', '.join(map(str, args))}")
+    return a - b
+
+def mul(a, b, *args):
+    logging.info(f"Mnożę: {a}, {b}, {', '.join(map(str, args))}")
+    return a * b * reduce(lambda x, y: x * y, args)
+
+def div(a, b, *args):
+    logging.info(f"Dzielę: {a}, {b}, {', '.join(map(str, args))}")
+    if b != 0:
+        return a / b
+    else:
+        raise ZeroDivisionError("Nie dziel przez 0!")
+
+def get_data(op):
+    a = int(input("Podaj składnik  1: "))
+    b = int(input("Podaj składnik  2: "))
+    args = []
+    if op in ["1", "3"]:
+        while True:
+            d = input("Podaj kolejną liczbę lub wpisz k by zakończyć: ")
+            if d == "k":
+                break
+            args.append(int(d))
+    return a, b, args
+
+operations = {
+    "1": add,
+    "2": sub,
+    "3": mul,
+    "4": div
+}
+
+if __name__ == "__main__":
+    op = input(
+        "Podaj działanie, posługując się odpowiednią liczbą: 1 Dodawanie, 2 Odejmowanie, 3 Mnożenie, 4 Dzielenie:"
+    )
+    a, b, args = get_data(op)
+
+    result = operations[op](a, b, *args)
+    print("Wynik to: ", result)
+
+logging.info("The program was called with this parameters %s" % op)
+logging.debug("First parameter is %s" % a)
+logging.debug("Second parameter is %s" % b)
